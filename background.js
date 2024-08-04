d0282501665b478dfae65657c23da796eb6a8ccc
chamaraserviceform <chamara@serviceform.com>
2024-08-04 09:42:22 +0530

    change selector for next button

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log("background listening started");
-
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    if (details.url.includes('netflix.com')) {
-      chrome.tabs.executeScript(details.tabId, {
-        file: 'content.js'
-      });
-    }
-  }, {url: [{urlMatches: 'https://www.netflix.com/*'}]});
--- content.js
+++ content.js
@@ -1,37 +1,48 @@
-console.log("content.js  aaa");
+console.log("content.js started");
+
 function clickButton(selector) {
-    const button = document.querySelector(selector);
-    if (button) {
-      button.click();
+  const button = document.querySelector(selector);
+  if (button) {
+    button.click();
+  }
+}
+
+function clickOnNextEpisode() {
+  const buttons = document.querySelectorAll('button');
+
+  const nextEpisodeButton = Array.from(buttons).find(button => button.textContent.includes("Next Episode"));
+
+  if (nextEpisodeButton) {
+    nextEpisodeButton.click();
+  }
+}
+
+// Selectors for Netflix buttons
+const skipButtonSelector = '.watch-video--skip-content-button';
+const continueWatchingButtonSelector = 'button[data-uia="interrupt-autoplay-continue"]';
+
+// Function to click buttons
+function clickNetflixButtons() {
+  clickButton(skipButtonSelector);
+  clickButton(continueWatchingButtonSelector);
+  clickOnNextEpisode()
+
+}
+
+// Monitor for DOM changes to detect new buttons
+const observer = new MutationObserver((mutations) => {
+  for (let mutation of mutations) {
+    if (mutation.type === 'childList' || mutation.type === 'subtree') {
+      clickNetflixButtons();
     }
   }
-  
-  // Selectors for Netflix buttons
-  const skipButtonSelector = '.watch-video--skip-content-button';
-  const nextEpisodeButtonSelector = 'button[data-uia="next-episode-seamless-button"]';
-  const continueWatchingButtonSelector = 'button[data-uia="interrupt-autoplay-continue"]';
-  
-  // Function to click buttons
-  function clickNetflixButtons() {
-    clickButton(skipButtonSelector);
-    clickButton(nextEpisodeButtonSelector);
-    clickButton(continueWatchingButtonSelector);
-  }
-  
-  // Monitor for DOM changes to detect new buttons
-  const observer = new MutationObserver((mutations) => {
-    for (let mutation of mutations) {
-      if (mutation.type === 'childList' || mutation.type === 'subtree') {
-        clickNetflixButtons();
-      }
-    }
-  });
-  
-  // Start observing
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-  
-  // Initial click in case buttons are already present
-  clickNetflixButtons();
+});
+
+// Start observing
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+// Initial click in case buttons are already present
+clickNetflixButtons();
